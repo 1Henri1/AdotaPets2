269728aede3666f2bd86ecd87f56d26f0845af8b
1henri1 <henri135246@gmail.com>
2023-05-11 21:13:35 -0300

    RUNNABLE

--- app/src/main/java/br/com/etecia/myapp/Splash_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Splash_Activity.java
@@ -2,7 +2,9 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Activity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class Splash_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        //  CRIANDO O CARREGAMENTO DA JANELA DE LOGIN
+        // HANDLER -> THREAD PROCESSO EM BACKGROUND
+
+        // INSTANCIAR UM RUNNABLE
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),
+                        Login_Activity.class));
+                finish(); // FECHA JANELA ATUAL
+            }
+        }, 3000);
     }
 }
